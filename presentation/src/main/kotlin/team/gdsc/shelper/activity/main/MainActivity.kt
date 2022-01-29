8f456c28f974f03b5a37482b19a90d9476013982
jisungbin <ji@sungb.in>
2022-01-29 23:03:41 +0900

    Update map

--- presentation/src/main/kotlin/team/gdsc/shelper/activity/main/MainActivity.kt
+++ presentation/src/main/kotlin/team/gdsc/shelper/activity/main/MainActivity.kt
@@ -112,7 +112,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun moveCameraAndZoom(locate: LatLng) {
         if (::map.isInitialized) {
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(locate, 100f))
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(locate, 50f))
         }
     }
 
