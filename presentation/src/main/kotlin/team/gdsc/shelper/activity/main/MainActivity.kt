89861c771c2dd170d19002f3479233496270fbd3
jisungbin <ji@sungb.in>
2022-01-29 22:14:48 +0900

    Update map

--- presentation/src/main/kotlin/team/gdsc/shelper/activity/main/MainActivity.kt
+++ presentation/src/main/kotlin/team/gdsc/shelper/activity/main/MainActivity.kt
@@ -11,20 +11,29 @@ package team.gdsc.shelper.activity.main
 
 // import team.gdsc.shelper.util.systemuicontroller.SystemUiController
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.birjuvachhani.locus.Locus
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.jisungbin.logeukes.LoggerType
+import io.github.jisungbin.logeukes.logeukes
 import team.gdsc.shelper.R
 import team.gdsc.shelper.activity.error.ErrorActivity
 import team.gdsc.shelper.util.NetworkUtil
 import team.gdsc.shelper.util.constant.IntentConstant
+import team.gdsc.shelper.util.extension.runIf
+import team.gdsc.shelper.util.systemuicontroller.setSystemBarsColor
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -46,8 +55,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
 
+        setSystemBarsColor(Color.TRANSPARENT)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        // SystemUiController(window).setSystemBarsColor(Color.TRANSPARENT)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -68,7 +77,36 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    private fun startLocationService() {
+        Locus.startLocationUpdates(this) { result ->
+            result.location?.let { location ->
+                val (latitude, longitude) = listOf(location.latitude, location.longitude)
+                marking(latitude, longitude)
+            }
+            result.error?.let { exception ->
+                logeukes(type = LoggerType.E) { exception }
+            }
+        }
+    }
+
+    private fun marking(latitude: Double, longitude: Double, title: String = "") {
+        if (::map.isInitialized) {
+            val locate = LatLng(latitude, longitude)
+            map.addMarker(
+                MarkerOptions()
+                    .position(locate)
+                    .runIf(title != "") { title(title) }
+            )
+            map.moveCamera(CameraUpdateFactory.newLatLng(locate))
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Locus.stopLocationUpdates()
     }
 }
