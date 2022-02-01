8cc7102ec3f6c3ce1b263bbb880aac9b2af9c80b
jisungbin <ji@sungb.in>
2022-02-01 16:43:15 +0900

    Add guide activity

--- app/src/main/kotlin/team/gdsc/shelper/activity/guide/GuideActivity.kt
+++ app/src/main/kotlin/team/gdsc/shelper/activity/guide/GuideActivity.kt
@@ -13,11 +13,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import team.gdsc.shelper.R
+import team.gdsc.shelper.activity.guide.model.GuideItem
 import team.gdsc.shelper.databinding.ActivityGuideBinding
 
 class GuideActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGuideBinding
+    private val guideAdapter by lazy { GuideAdpater() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,38 @@ class GuideActivity : AppCompatActivity() {
 
         binding.ivBack.setOnClickListener {
             finish()
+        }
+
+        binding.rvGuide.apply {
+            setHasFixedSize(true)
+            adapter = guideAdapter.apply {
+                setHasStableIds(true)
+                submitItems(getFilteredItem())
+            }
+        }
+
+        binding.cgContainer.setOnCheckedChangeListener { _, checkedId ->
+            val item = getFilteredItem(
+                when (checkedId) {
+                    R.id.chip_flood -> "홍수"
+                    R.id.chip_volcano -> "화산"
+                    R.id.chip_typhoon -> "태풍"
+                    R.id.chip_earthquake -> "지진"
+                    R.id.chip_heat_wave -> "폭염"
+                    R.id.chip_heavy_snow -> "폭설"
+                    R.id.chip_heavy_rain -> "호우"
+                    R.id.chip_tsunami -> "쓰나미"
+                    else -> "전체"
+                }
+            )
+            guideAdapter.submitItems(item)
+        }
+    }
+
+    private fun getFilteredItem(disasterName: String = "전체") = GuideItem.getAll().run {
+        when (disasterName) {
+            "전체" -> this
+            else -> filter { it.title == disasterName }
         }
     }
 }
---
+++ app/src/main/kotlin/team/gdsc/shelper/activity/guide/GuideAdpater.kt
@@ -0,0 +1,56 @@
+/*
+ * Shelper © 2022 Team Helper. all rights reserved.
+ * Shelper license is under the MIT.
+ *
+ * [ActionGuidelineAdapter.kt] created by Ji Sungbin on 22. 2. 1. 오후 4:32
+ *
+ * Please see: https://github.com/gdsckoreahackathon2022/01_Helper/blob/main/LICENSE.
+ */
+
+package team.gdsc.shelper.activity.guide
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import team.gdsc.shelper.R
+import team.gdsc.shelper.activity.guide.model.GuideItem
+import team.gdsc.shelper.databinding.LayoutGuideItemBinding
+
+class GuideAdpater : RecyclerView.Adapter<GuideAdpater.ActionGuidelineHolder>() {
+
+    private var items = emptyList<GuideItem>()
+
+    class ActionGuidelineHolder(private val binding: LayoutGuideItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(model: GuideItem) {
+            binding.item = model
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActionGuidelineHolder {
+        return ActionGuidelineHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.layout_guide_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitItems(items: List<GuideItem>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ActionGuidelineHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+}
