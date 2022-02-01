ea267b1671ec5bfcb0b3cda0c05c8514d944a79c
jisungbin <ji@sungb.in>
2022-02-01 20:48:37 +0900

    Add message activity

---
+++ app/src/main/kotlin/team/gdsc/shelper/activity/message/DataStore.kt
@@ -0,0 +1,14 @@
+/*
+ * Shelper © 2022 Team Helper. all rights reserved.
+ * Shelper license is under the MIT.
+ *
+ * [DataStore.kt] created by Ji Sungbin on 22. 2. 1. 오후 8:45
+ *
+ * Please see: https://github.com/gdsckoreahackathon2022/01_Helper/blob/main/LICENSE.
+ */
+
+package team.gdsc.shelper.activity.message
+
+object DataStore {
+    val contents = ""
+}
